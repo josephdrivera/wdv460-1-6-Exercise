68766ba2344900993b3bb928f7639f60db877667
Joseph D Rivera <josephdrivera15@gmail.com>
2023-04-10 09:10:51 -0400

    Static website finished

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Hello, this is my static page in Next.js!</h1>
+            <p>Welcome to my website.</p>
+        </div>
+    );
+};
+
+export default HomePage;
